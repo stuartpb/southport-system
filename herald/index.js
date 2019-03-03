31b32bb360b862ce37779f6a5d55b9a065d9597f
Stuart P. Bentley <stuart@testtrack4.com>
2019-03-03 17:59:53 +0000

    Rename services to better describe roles

--- herald/index.js
+++ herald/index.js
@@ -1,6 +1,13 @@
 const Gpio = require('onoff').Gpio;
+
+// Relay for ringing the doorbell.
 const relay = new Gpio(4, 'high', {activeLow: true});
+
+const morgan = require('morgan');
+
 const app = require('express')();
+
+app.use(morgan('[:date[iso]] :remote-addr ":method :url HTTP/:http-version" :status :res[content-length]'));
 
 function ringBell(duration) {
   return new Promise((resolve, reject) => {
@@ -14,9 +21,12 @@ function ringBell(duration) {
   });
 }
 
-app.post('/ring',(req, res) => {
-  console.log('ring received');
+app.post('/ring', (req, res) => {
   ringBell(req.query.duration).then(() => res.send());
+})
+
+app.post('/present/still', (req, res) => {
+  // TODO: implement
 })
 
 app.listen(80);
