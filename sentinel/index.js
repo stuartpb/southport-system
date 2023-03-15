2a309814e2a0f3c498d85d85b714ffc14de343c1
Stuart P. Bentley <s@stuartpb.com>
2023-03-15 00:32:54 +0000

    Add email notification functionality

--- sentinel/index.js
+++ sentinel/index.js
@@ -2,17 +2,33 @@ const Gpio = require('onoff').Gpio;
 const fetch = require('node-fetch');
 const debounce = require('lodash.debounce');
 const sensor = new Gpio(3, 'in', 'both');
+const { v4: uuid } = require('uuid');
+const nodemailer = require("nodemailer");
 
 function ensureHttp(address) {
   if (!/https?:/.test(address)) address = 'http://' + address;
   return address;
 }
 
-const HERALD_ENDPOINT = ensureHttp(process.env.HERALD_ENDPOINT || 'localhost');
+const HERALD_ENDPOINT =
+  ensureHttp(process.env.HERALD_ENDPOINT || 'localhost');
 const SNAP_URL = process.env.SNAP_URL;
+
 const RING_DURATION = process.env.RING_DURATION;
 const STILL_DURATION = process.env.STILL_DURATION;
 const DEBOUNCE_TIME = process.env.DEBOUNCE_TIME || 0;
+
+const EMAIL_SENDER_ADDRESS = process.env.EMAIL_SENDER_ADDRESS;
+const EMAIL_SENDER_NAME =
+  process.env.EMAIL_SENDER_NAME || "Southport Driveway";
+const EMAIL_RECIPIENT = process.env.EMAIL_RECIPIENT;
+const EMAIL_ATTACHMENT_FILENAME =
+  process.env.EMAIL_ATTACHMENT_FILENAME || "snap.jpg";
+
+const SMTP_HOSTNAME = process.env.SMTP_HOSTNAME;
+const SMTP_PORT = parseInt(process.env.SMTP_PORT) || 465;
+const SMTP_USERNAME = process.env.SMTP_USERNAME || EMAIL_SENDER_ADDRESS;
+const SMTP_PASSWORD = process.env.SMTP_PASSWORD;
 
 const ringEdge = 0; // ring on falling edge
 
@@ -32,9 +48,43 @@ function notifyHerald() {
   return Promise.all(fetches.map(p=>p.catch(console.error)));
 }
 
-const sensorTriggered = debounce(notifyHerald, DEBOUNCE_TIME, {
+const mailer = nodemailer.createTransport({
+  host: SMTP_HOSTNAME,
+  port: SMTP_PORT,
+  auth: {
+    user: SMTP_USERNAME,
+    pass: SMTP_PASSWORD,
+  },
+});
+
+function sendEmailNotification() {
+  const cid = uuid();
+  return mailer.sendMail({
+    from: `${JSON.stringify(EMAIL_SENDER_NAME)} <${EMAIL_SENDER_ADDRESS}>`,
+    to: EMAIL_RECIPIENT,
+    subject: `Vehicle Detected from ${
+      EMAIL_SENDER_NAME} at ${new Date().toLocaleString()}`,
+    text: "Image attached.",
+    html: `<img src="cid:${cid}">`,
+    attachments: [{
+      filename: EMAIL_ATTACHMENT_FILENAME,
+      path: SNAP_URL,
+      cid }]
+  });
+}
+
+function postNotifications() {
+  const notifications = [notifyHerald()];
+
+  if (EMAIL_RECIPIENT)
+    notifications.push(sendEmailNotification());
+  
+  return Promise.all(notifications);
+}
+
+const sensorTriggered = debounce(postNotifications, DEBOUNCE_TIME, {
   leading: true, trailing: false, maxWait: DEBOUNCE_TIME
-})
+});
 
 sensor.watch((err, value) => {
   if (err) {
